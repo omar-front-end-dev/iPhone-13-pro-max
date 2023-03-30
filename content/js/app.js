e4f03f075ae02cd3d706cb210ad61e7cab6b20f3
omar-dev <om7994079@gmail.com>
2023-03-31 01:51:27 +0200

    I learned to switch the image in this project

---
+++ content/js/app.js
@@ -0,0 +1,10 @@
+let mainImg = document.getElementById("main--img");
+let body = document.querySelector("body");
+
+function phones (phone){
+    mainImg.src = phone;
+};
+
+function colors (color){
+    body.style.background = color
+}
